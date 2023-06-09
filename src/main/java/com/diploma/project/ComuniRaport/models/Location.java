2b984521395710e2125cb0f228c7bf6bbac374d0
Alin Vujde <>
2023-06-09 13:17:14 +0300

    Reports Added

---
+++ src/main/java/com/diploma/project/ComuniRaport/models/Location.java
@@ -0,0 +1,15 @@
+package com.diploma.project.ComuniRaport.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+    private long latitude;
+    private long longitude;
+}
